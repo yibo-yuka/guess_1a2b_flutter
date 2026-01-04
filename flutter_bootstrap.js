242f32c28e77ac2c82930bab906e62cd4d1f70ad
yibo-yuka <yibo-yuka@users.noreply.github.com>
2026-01-04 08:55:19 +0000

    deploy: 8275d61f75b7c38e6ad7cda0277c3f63c5966f6b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2762273512"
+    serviceWorkerVersion: "2667464030"
   }
 });
